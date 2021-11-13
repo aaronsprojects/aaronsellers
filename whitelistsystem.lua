5b9fd334684584784a18007f716391360c129fbf
random <78721749+LPrandom@users.noreply.github.com>
2021-11-12 21:05:58 -1000

    Update whitelistsystem.lua

--- whitelistsystem.lua
+++ whitelistsystem.lua
@@ -1,1 +1,28 @@
-print("fatherless chil lel")
+local joemomma = {
+  2721170080,
+}
+
+local mods = {}
+
+local player = game.Players.LocalPlayer
+local function kfcshop()
+	for i,v in pairs(game.Players:GetChildren()) do
+		if table.find(joemomma,v.UserId) then
+			if v.Character then
+				v.Character:FindFirstChild("Humanoid").DisplayName = '[⭐]'..v.DisplayName
+			end
+		elseif table.find(joemomma,v.UserId) then
+			if v.Character then
+				v.Character:FindFirstChild("Humanoid").DisplayName = '[👑]'..v.DisplayName
+			end
+		else
+			if v.Character then
+				if not v.Character.Head:FindFirstChild("OriginalSize") then
+					v.Character:FindFirstChild("Humanoid").DisplayName = '[🐔]'..v.DisplayName
+				end
+			end
+		end
+  end
+end
+local success, error = pcall(kfcshop)
+return joemomma
